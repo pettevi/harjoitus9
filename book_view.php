435dcaa44ca0f28e04cef62d560a58bf42567619
petteri <pete.hamalainen@gmail.com>
2018-09-18 10:47:26 +0300

    muotoili

--- book_view.php
+++ book_view.php
@@ -5,8 +5,19 @@
 <body>
     <h2>Books from the database</h2>
     <?php
-    function show_books($a){
+    function show_books($a)
+    {
+      foreach ($a as $key => $value)
+      {
+        echo "ID ".a['book_id']."<br>";
+        echo "Nimi ".a['book_name']."<br>";
+        echo "Kirjailija ".a['author']."<br>";
+        echo "ISBN ".a['isbn']."<br>";
+      }
+
+      <p>
         print_r($a);
+      </p>
     }
  ?>
 </body>
